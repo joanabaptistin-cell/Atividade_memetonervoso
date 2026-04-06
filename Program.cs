8ec8a0efbfc98d57b618445e2fed773efd090e65
Joana Baptistin <joana.baptistin@gmail.com>
2026-04-05 22:30:47 -0300

    atividade meme

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string fraseMemeTonervoso,fraseDigitada;
+
+Console.Write("Usuário, o que você pensa sobre mim?");
+fraseDigitada = Console.ReadLine()!;
+
+fraseMemeTonervoso 
+= $"{fraseDigitada.Remove(10)}...NÃO,PERA. \nTô Nervoso";
+ 
+Console.Write(fraseMemeTonervoso);
